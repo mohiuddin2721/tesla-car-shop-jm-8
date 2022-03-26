3dba92e13709ad42e33a1e864b217816706d86bd
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-03-26 18:47:13 +0600

    remove button added

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -4,13 +4,24 @@ import './Cart.css';
 const Cart = (props) => {
     console.log(props.cart);
     const {picture, name} = props.cart;
+    
+    let pictures = {};
+    let names = {};
+
+
+    const removeItems = () => {
+        document.getElementById('selected-car').innerHTML = '';
+    }
     return (
         <div className='cart'>
             <h3>Selected Cars</h3>
-            <p><img className='selected-img' src={picture} alt="" /> {name}</p>
+            <ul className='selected-cars' id='selected-car'>
+                <img className='selected-img' src={picture} alt="" /> {name}
+            </ul>
+            
             <button><p className='choose-one-btn'>CHOOSE 1 FOR ME</p></button>
             <br />
-            <button><p className='select-again-btn'>SELECT AGAIN</p></button>
+            <button onClick={removeItems}><p className='select-again-btn'>SELECT AGAIN</p></button>
         </div>
     );
 };
