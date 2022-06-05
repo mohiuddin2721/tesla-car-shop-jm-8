935a7ea19a3d6b26e13160375b0d2f5900528214
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-06-06 01:53:20 +0600

    headline added

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -4,6 +4,7 @@ import './Header.css';
 const Header = () => {
     return (
         <div className='header-title'>
+            {/* headline */}
             <h1>Tesla Car House</h1>
             <h3>Choose 4 cars</h3>
         </div>
